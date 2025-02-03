9c91df81c61380f872fb6e0eab9fea97375a7a67
johnb08 <johnb08.kodehode@gmail.com>
2025-02-03 10:37:46 +0100

    comments

--- EventSignupApi/Program.cs
+++ EventSignupApi/Program.cs
@@ -9,11 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Servicene her har ingen state knyttet direkte til seg, de representerer en samling metoder som skal kjøres på en eller flere modeller. De kan derfor implementeres som en transient service.
+//dvs vi lager og disposer en instans hver gang vi kaller den. 
 builder.Services.AddTransient<DTOService>();
 builder.Services.AddTransient<EventDataHandler>();
 builder.Services.AddTransient<UserDtoService>();
 builder.Services.AddTransient<UserHandler>();
+
+//TokenService skal holde oversikt over aktive sessions, denne staten skal helst holdes i livet over hele livssyklussen til appen vår, og bør derfor implementeres som en singleton.
 builder.Services.AddSingleton<TokenService>();
+
+
 builder.Services.AddLogging(builder => builder.AddConsole());
 /* Her lager vi en Service som skal kunne levere en context av databasen vår som kan injectes til controllerene våre senere. */
 builder.Services.AddDbContext<DatabaseContext>();
