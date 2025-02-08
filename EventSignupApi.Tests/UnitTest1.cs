b9c448957578e45cef3cc062d8e11ab91055a654
johnb08 <johnb08.kodehode@gmail.com>
2025-02-08 16:14:44 +0100

    Added Wikipedia Levenshtein Two Matrix Implementation

--- EventSignupApi.Tests/UnitTest1.cs
+++ EventSignupApi.Tests/UnitTest1.cs
@@ -9,11 +9,24 @@ public class UnitTest1
     [InlineData("", "", 0)]
     [InlineData("a", "", 1)]
     [InlineData("", "a", 1)]
-    public void Distance_ExpectedResults(string source, string target, int expected)
+    public void DistanceRec_ExpectedResults(string source, string target, int expected)
     {
         var ls = new LS();
-        int actual = ls.Distance(source.AsSpan(), target.AsSpan());
+        int actual = ls.DistanceRec(source.AsSpan(), target.AsSpan());
         Assert.Equal(expected, actual); 
+    }
+
+    [Theory]
+    [InlineData("kitten", "sitting", 3)]
+    [InlineData("flaw", "lawn", 2)]
+    [InlineData("", "", 0)]
+    [InlineData("a", "", 1)]
+    [InlineData("", "a", 1)]
+    public void DistanceIter_ExpectedResults(string source, string target, int expected)
+    {
+        var ls = new LS();
+        int actual = ls.DistanceIter(source.AsSpan(), target.AsSpan());
+        Assert.Equal(expected, actual);
     }
     
 }
--- EventSignupApi/Services/LevenShteinService/LS.cs
+++ EventSignupApi/Services/LevenShteinService/LS.cs
@@ -14,7 +14,7 @@ public class LS
     /// <param name="source">string a</param>
     /// <param name="target">string b</param>
     /// <returns></returns>
-    public int Distance(ReadOnlySpan<char> source, ReadOnlySpan<char> target)
+    public int DistanceRec(ReadOnlySpan<char> source, ReadOnlySpan<char> target)
     {
         // a.Length, eller som på matematisk: |a|
         int sourceLength = source.Length;
@@ -31,7 +31,7 @@ public class LS
         //Hvis a.head = b.head, return lev(a.tail, b.tail)
         if (source[0] == target[0])
         {
-            return Distance(source[1..], target[1..]);
+            return DistanceRec(source[1..], target[1..]);
         }
 
         /* 
@@ -42,9 +42,9 @@ public class LS
                 Lev(a.tail, b.tail)
          */
         return 1 + Min(
-            Distance(source[1..], target),
-            Distance(source, target[1..]),
-            Distance(source[1..], target[1..])
+            DistanceRec(source[1..], target),
+            DistanceRec(source, target[1..]),
+            DistanceRec(source[1..], target[1..])
         );
     }
     //En hjelpemetode for å returnere minimumverdien av tre integers. Kan også bruke en Math.Min chain. 
@@ -54,5 +54,66 @@ public class LS
         if (b < c) min = b < a ? b : a;
         else min = c < a ? c : a;
         return min;
+    }
+
+    /// <summary>
+    /// En implementasjon av Two Matrix Row Levenshtein fra wikipedia
+    /// https://en.wikipedia.org/wiki/Levenshtein_distance
+    /// Lager to vektorer, itererer gjennom disse mens den sammenligner karakterer i
+    /// source og target.
+    /// hvis source er større en target, blir disse flippet.
+    /// </summary>
+    /// <param name="source">source string som skal sammenlignes med et target</param>
+    /// <param name="target">target som skal sammenlignes med source</param>
+    /// <returns>Levenshtein distansen mellom to strings</returns>
+    public int DistanceIter(ReadOnlySpan<char> source, ReadOnlySpan<char> target)
+    {
+        //Siden denne implementasjonen krever at source er mindre eller lik i lengde på target,
+        //Må vi potensielt flippe disse hvis source er lengre enn target. Distansen er lik uansett.
+        var accTarget = source.Length < target.Length ? source : target;
+        var accSource = source.Length < target.Length ? target : source;
+
+
+        //Vi initialiserer to vektorer som skal brukes for å regne ut avstanden mellom source og target.
+        Span<int> v0 = stackalloc int[accTarget.Length + 1];
+        Span<int> v1 = stackalloc int[accTarget.Length + 1];
+
+        //Vi setter alle elementene i vektor 0 til index i, som skal brukes som basis for hvor mye det "koster" å appende en
+        //bokstav fra en gitt posisjon i, fra target til source for å gjøre source lik target. 
+        for (int i = 0; i < accTarget.Length; i++)
+        {
+            v0[i] = i;
+        }
+
+
+
+        for (int i = 0; i < accSource.Length; i++)
+        {   
+
+            //Vi setter så første element i v1 lik i +1, for å vise "kosten" for å slette et element i source for å gjøre lik target.
+            v1[0] = i + 1;
+
+
+            //Vi bruker så formelen for å fylle inn resten av kosten for karakter s[i] sammenlignet med t[j]
+            for (int j = 0; j < accTarget.Length; j++)
+            {
+                int deletionCost = v0[j + 1] + 1;
+                int insertCost = v1[j] + 1;
+                int subCost;
+                if (accSource[i] == accTarget[j])
+                {
+                    subCost = v0[j];
+                }
+                else subCost = v0[j] + 1;
+
+
+                v1[j + 1] = Min(deletionCost, insertCost, subCost);
+            }
+
+            //vi swapper så verdien fra v1 til v0, for å beholde utreningen før neste step.
+            v1.CopyTo(v0);      
+        }
+        //Etter siste swappet, er hele distansen flyttet til index = t.Length i v0, og vi kan returnere den ut. 
+        return v0[accTarget.Length];
     }
 }
